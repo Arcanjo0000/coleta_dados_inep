2ce6ec30cd4e1ced5106cee6fb61cb8ff2b7e70c
Arcanjo0 <archanjo2447@gmail.com>
2024-05-19 23:24:31 -0300

    construindo o tratamento de dados

--- main.py
+++ main.py
@@ -1,8 +1,36 @@
+import tkinter as tk
+import customtkinter as ctk
 from classe.tabela import Tabela
 from script.pegando_caminho_tabelas import pergunta
 from script.lendo_json import ler_caminhos, ler_estrutura
 from script.abrindo_tabelas import acessando_os_caminhos, acessando_tabelas
-from script.tratamento_de_dados import tratando_dados
+from script.tratamento_de_dados import pegando_arquivos_com_dados
+
+def confirmar(janela):
+    fechar_janela(janela)
+    pegando_arquivos_com_dados()
+
+def fechar_janela(janela):
+    janela.destroy()
+
+def encerrar(janela):
+    fechar_janela(janela)
+    print("encerrando")
+    exit()
+
+def continuar():
+    ctk.set_appearance_mode("System")
+    ctk.set_default_color_theme("blue")
+
+    janela = ctk.CTk()
+    janela.geometry("780x480")
+    janela.title("deseja comtinuar?")
+    
+    ctk.CTkButton(janela, "Sim", command=lambda:confirmar(janela))
+    ctk.CTkButton(janela, "Não", command=lambda:encerrar(janela))
+
+    janela.mainloop()
+
 
 
 def main():
@@ -13,7 +41,7 @@ def main():
     caminhos_utilizaveis = acessando_os_caminhos()
     acessando_tabelas(caminhos_utilizaveis)
     
-    tratando_dados()
+    
 
 
 if __name__ == "__main__":
--- script/tratamento_de_dados.py
+++ script/tratamento_de_dados.py
@@ -1,24 +1,64 @@
 import os
+import pandas as pd
 
-def tratando_dados():
+
+def pegando_arquivos_com_dados():
     
     arquivos_com_dados = os.listdir("informacoes/data")
     for ano in range(2022, 2008, -1):
         for arquivo in arquivos_com_dados:
             estrutura_antiga = ano < 2021
+            estrutura_nova = ano >+ 2021
             if str(arquivo) in str(ano):
                 if estrutura_antiga:
                     caminho_arquivo = os.path.join("informacoes/data", arquivo)
-                    with open(str(arquivo), "r") as file:
+                    with open(str(caminho_arquivo), "r") as file:
                         dados_do_arquivo = []
                         for linha in file:
-                            dados_do_arquivo.append(linha)
+                            linha_limpa = linha.strip()
+                            dados_do_arquivo.append(linha_limpa)
+                        tratar_dados_antigos(dados_do_arquivo)
+                elif estrutura_nova:
+                    caminho_arquivo = os.path.join("informacoes/data", arquivo)
+                    with open(str(caminho_arquivo), "r") as file:
+                        dados_do_arquivo = []
+                        for linha in file:
+                            linha_limpa = linha.strip()
+                            dados_do_arquivo.append(linha_limpa)
+                        tratar_dados_novos(dados_do_arquivo)
+            
 
-                        
-                        criar_csv_antigo(dados_do_arquivo)
+def tratar_dados_antigos(lista_de_dados):
+    lista_com_dados_tratados = []
+    for idx, dado in enumerate(lista_de_dados):
+        if idx % 2 == 0:
+            total = lista_de_dados[idx-1]
+            total_privada = lista_de_dados[idx]
+            total_publica = total - total_privada
+            lista_com_dados_tratados.extend([total_publica, total_privada])
+    adicionar_na_tabela(lista_com_dados_tratados)
 
-def criar_csv_antigo():
-    pass
+def tratar_dados_novos(lista_de_dados):
+    lista_com_dados_tratados = []
+
+
+
+def criar_tabela():
+    dados = {}
+    df = pd.DataFrame(dados)
+    df.to_excel("dados_tratados.xlsx", index=False)
+
+
+def adicionar_na_tabela(dados_tratados):
+    tabela_existente = pd.read_excel("informacoes/data/dados_tratados.xlsx")
+    nova_linha = {}
+    df_novo = pd.DataFrame(nova_linha)
+    df_atualizado = pd.concat([tabela_existente, df_novo], ignore_index=True)
+
+    df_atualizado.to_excel("informacoes/data/dados_tratados.xlsx")
+
+
+
 
             
             
