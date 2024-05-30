cb1677fe2caf5ff2824cde848981f6922d24dd44
Arcanjo0 <archanjo2447@gmail.com>
2024-05-30 00:35:11 -0300

    andamento no tratamento de dados

--- script/tratamento_de_dados.py
+++ script/tratamento_de_dados.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
 from itertools import islice
+import more_itertools as mit
 
 def pegando_arquivos_com_dados():
     #lista com os arquivo
@@ -12,124 +13,128 @@ def pegando_arquivos_com_dados():
         estrutura_antiga = ano < 2021
         estrutura_nova = ano >= 2021
         estrutura_bem_antiga = ano == 2009
-        lista_com_7 = []
-        lista_sem_7 = []
-        lista_publica = []
-        lista_privada = []
         
         caminho_arquivo = os.path.join("informacoes/data", arquivo)
         #abre os arquivos
-        with open(str(caminho_arquivo), "r") as file:
+        dados_do_arquivo = lendo_arquivo(caminho_arquivo)
+
+        dados_transformados = tranformando_dados(dados_do_arquivo)
+
+
+        if estrutura_nova:
+            separando_modalidades_novo(dados_transformados, ano)
+            
+
+def tranformando_dados(dados_do_arquivo):
+    lista_com_dados_como_numero = []
+    for idx, dado_list in enumerate(dados_do_arquivo):
+        dado_str = dado_list[0]
+        dado_int = int(dado_str.replace(".",""))
+        lista_com_dados_como_numero.append(dado_int)
+    return lista_com_dados_como_numero
+
+def lendo_arquivo(caminho_arquivo):
+    with open(str(caminho_arquivo), "r") as file:
             dados_do_arquivo = []
             for linha in file:
                 linha_limpa = linha.split()
                 #tira os espaços das linhas
                 if linha_limpa:
                     dados_do_arquivo.append(linha_limpa)
-        lista_com_dados_como_numero = []
-        for idx, dado_list in enumerate(dados_do_arquivo):
-            dado_str = dado_list[0]
-            dado_int = int(dado_str.replace(".",""))
-            lista_com_dados_como_numero.append(dado_int)
+            return dados_do_arquivo
+
+def separando_modalidades_novo(lista_com_dados_int, ano):
+    print()
+    print(f"ano = {ano}")
+    struct = 0
+    iterador = iter(lista_com_dados_int)
+    presencial = list(islice(iterador, 84))
+    ead = list(iterador)
+    # pegar o presencial e dar um jeito de separar os cursos
+    cursos_presenciais = mit.chunked(presencial, 21)
+    #o idx é o código do curso
+    # presenciais
+    for idx, curso in enumerate(cursos_presenciais):
+        modalidade = "presencial"
+        match idx:
+            case 0:
+                codigo_curso = idx
+                separando_tipos_de_dados(curso, codigo_curso, ano, struct, modalidade)
+            case 1:
+                codigo_curso = idx
+                separando_tipos_de_dados(curso, codigo_curso, ano, struct, modalidade)
+            case 2:
+                codigo_curso = idx
+                separando_tipos_de_dados(curso, codigo_curso, ano, struct, modalidade)
+            case 3:
+                codigo_curso = idx
+                separando_tipos_de_dados(curso, codigo_curso, ano, struct, modalidade)
+            case _ :
+                print("erro ao tentar separar os cursos presenciais")
+    
+    cursos_ead = mit.chunked(ead, 21)
+    for idx, curso in enumerate(cursos_ead):
+        modalidade = "EAD"
+        match idx:
+            case 0:
+                codigo_curso = idx
+                separando_tipos_de_dados(curso, codigo_curso, ano, struct, modalidade)
+            case 1:
+                codigo_curso = idx
+                separando_tipos_de_dados(curso, codigo_curso, ano, struct, modalidade)
+            case 2:
+                codigo_curso = idx
+                separando_tipos_de_dados(curso, codigo_curso, ano, struct, modalidade)
+            case 3:
+                codigo_curso = idx
+                separando_tipos_de_dados(curso, codigo_curso, ano, struct, modalidade)
+            case _ :
+                print("erro ao tentar separar os cursos EAD")
 
 
-        if estrutura_nova:
-            # tenho que separar por curso ano pub.pre pub.ead priv.pre priv.ead
-            iterador = iter(lista_com_dados_como_numero)
-            presencial = list(islice(iterador, 84))
-            ead = list(iterador)
-            print()
-            print(f"ano = {ano}")
-            print()
-            print(f"dados presenciais: \n{presencial}")
-            print()
-            print(f"dados ead: \n{ead}")
-            # deu certo 🥳🤓
-            
+def separando_tipos_de_dados(dados_do_curso, codigo_curso, ano, struct, modalidade):
+    if struct == 0:
 
+        dados_separados = mit.chunked(dados_do_curso, 7)
+        print()
+        print(modalidade)
+        print(f"curso = {codigo_curso}")
+        for qual_dado, dados in enumerate(dados_separados):
+            match qual_dado:
+                case 0:
+                    tipo_dado = "cursos"
+                    tratar_dados_novos(tipo_dado, dados, ano, codigo_curso, modalidade)
+                case 1:
+                    tipo_dado = "matriculas"
+                    tratar_dados_novos(tipo_dado, dados, ano, codigo_curso, modalidade)
+                case 2:
+                    tipo_dado = "concluintes"
+                    tratar_dados_novos(tipo_dado, dados, ano, codigo_curso, modalidade)
+                case _:
+                    print("erro ao separar os tipos de dados")
 
+def tratar_dados_novos(tipo_dado, dados, ano, codigo_curso, modalidade):
+    print()
+    print(f"tipo de dado = {tipo_dado}")
+    publica = []
+    privada = []
 
+    for idx, dado in enumerate(dados):
+        posicao = idx + 1
 
-            # aqui tem que ter uma loop para cada curso, mas como eu faço isso?
-            # for curso in cursos:
-            #     print()
-            #     print(f"curso = {curso}")
-            #     # separa os dados dos arquivos em duas listas 
-            #     for idx, dado in enumerate(dados_do_arquivo):
-            #         print(f"dado = {idx}")
-            #         dado_com_ponto = dado[0]
-            #         dado_sem_ponto = dado_com_ponto.replace(".", "")
-            #         dado = int(dado_sem_ponto)
-            #         print(dado)
-
-            #         if (idx + 1) % 7 == 0:
-            #             lista_com_7.append(int(dado))
-            #         else:
-                        
-            #             lista_sem_7.append(int(dado))
-                
-
-            #     for idx, dado in enumerate(lista_sem_7):
-            #         if (idx + 1) % 2 == 0:
-            #             # total e priavda estão sendo tratados como lista. por que?
-
-            #             total = lista_sem_7[idx - 1]
-            #             privada = lista_sem_7[idx]
-            #             publica = total - privada
-            #             lista_publica.append(publica)
-            #             lista_privada.append(privada)
-            #         elif (idx + 1) % 7 == 0:
-            #             posicao = idx + 1
-            #             match posicao:
-            #                 case 7:
-            #                     lista_publica.append(lista_com_7[0])
-            #                 case 14:
-            #                     lista_publica.append(lista_com_7[1])
-            #                 case 21:
-            #                     lista_publica.append(lista_com_7[2])
-                    
-                    
-                
-
-            
-
-
-
-
-        # if estrutura_antiga:
-        #     caminho_arquivo = os.path.join("informacoes/data", arquivo)
-        #     with open(str(caminho_arquivo), "r") as file:
-        #         dados_do_arquivo = []
-        #         for linha in file:
-        #             linha_limpa = linha.strip()
-        #             dados_do_arquivo.append(linha_limpa)
-        #         tratar_dados_antigos(dados_do_arquivo)
-        # elif estrutura_nova:
-        #     caminho_arquivo = os.path.join("informacoes/data", arquivo)
-        #     with open(str(caminho_arquivo), "r") as file:
-        #         dados_do_arquivo = []
-        #         for linha in file:
-        #             linha_limpa = linha.strip()
-        #             dados_do_arquivo.append(linha_limpa)
-        #         tratar_dados_novos(dados_do_arquivo)
-        # elif estrutura_bem_antiga:
-
-        # else:
-        #     print("invalido")
-            
-
-def tratar_dados_antigos(lista_de_dados):
-    lista_com_dados_tratados = []
-    for idx, dado in enumerate(lista_de_dados):
-        if idx % 2 == 0:
-            total = lista_de_dados[idx-1]
-            total_privada = lista_de_dados[idx]
+        if posicao % 2 == 0:
+            total = dados[idx - 1]
+            total_privada = dado
             total_publica = total - total_privada
-            lista_com_dados_tratados.extend([total_publica, total_privada])
-    adicionar_na_tabela(lista_com_dados_tratados)
+            publica.append(total_publica)
+            privada.append(total_privada)
+        elif posicao % 7 == 0:
+            publica.append(dado)
+    soma_publica = sum(privada)
+    soma_privada = sum(publica)
+    print(f"privada = {soma_privada}")
+    print(f"publica = {soma_publica}")
 
-def tratar_dados_novos(lista_de_dados):
-    lista_com_dados_tratados = []
 
 
 
